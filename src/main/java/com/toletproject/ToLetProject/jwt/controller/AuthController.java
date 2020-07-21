998464146a5dd1d8382eec8e313595f36d3c67f7
Abdullah Al Habib <41731413+aalhabib001@users.noreply.github.com>
2020-07-22 04:18:55 +0600

    Update AuthController.java

--- src/main/java/com/toletproject/ToLetProject/jwt/controller/AuthController.java
+++ src/main/java/com/toletproject/ToLetProject/jwt/controller/AuthController.java
@@ -91,7 +91,7 @@ public class AuthController {
 //        return forgetPasswordService.sendMail();
 //    }
 
-    @GetMapping("/LUser")
+    @GetMapping("/RUser")
     public String getSomething() {
         return "NULL";
     }
